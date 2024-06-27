715f181ca9d4343d48bd08f383052d072ace7766
SonamSaraswat <sonamsaraswat777@gmail.com>
2024-06-27 15:03:00 +0530

    datatypes

---
+++ Datatypes.js
@@ -0,0 +1,19 @@
+
+let n=19;
+let name="sonam"
+let isloggedIn=true
+
+//primitive data type
+
+//number => 2 to 53
+//bigint 
+//string =>" "
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+// symbol =>unique
+
+
+/*Interview question*/
+console.log(typeof null)
+console.log(undefined)
