22285e46d10a952c1aad4c67bbaeeed5257fc352
SonamSaraswat <sonamsaraswat777@gmail.com>
2024-06-28 16:45:27 +0530

    arrays

--- Arrays.js
+++ Arrays.js
@@ -47,5 +47,41 @@ son.shift(7);
 console.log(son);
 
 
+let a1=[1,2,3,4];
+let a2=[5,6,7,8];
+
+//a1.push(a2);
+//console.log(a1);
+
+//concat
+let a3=a1.concat(a2);
+console.log(a3);
+
+//spread
+//let a3 = [...a1, ...a2];
+//console.log(a3);
+
+//flat
+let a5=[1,2,3,[4,5,6],[4,5,[6,7],8]];
+let a6=a5.flat(Infinity);
+console.log(a6);
+
+//convert array from strings and variables
+
+let score1=6;
+let score2=7;
+let score3=8;
+let score4=9;
+let a4=Array.of(score1,score2,score3,score4)
+console.log(a4);
+
+console.log(Array.isArray("Sonam"))
+let s=(Array.from("Sonam"))
+console.log(s);
+//console.log(Array.from{name:"Sonam"})//Interesting read more
+
+
+
+
 
 
