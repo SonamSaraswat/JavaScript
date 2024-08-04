883d6898e2a0172ca086e9daeb78dfb551bda77e
SonamSaraswat <sonamsaraswat777@gmail.com>
2024-08-04 14:08:42 +0530

    New Changes

--- Practice/Promises.js
+++ Practice/Promises.js
@@ -5,6 +5,27 @@ const promiseOne=new Promise(function(resolve, reject){
     },1000)
 })
 promiseOne.then(function(){
-
     console.log("promise 1 resolved")
+})
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve();
+        console.log("Async task2")
+    })
+}).then(function(){
+    console.log("promise 2get executed")
+})
+
+const promisethree=new Promise(function(resolve,reject){
+
+    let error=false;
+    setTimeout(function(){
+        if(error){
+        console.log("Promise 3 resolved")
+        }
+        else{
+            console.log("Error")
+        }
+    },1000)
 })
